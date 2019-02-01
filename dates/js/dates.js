6efaa405ce40f36639e0f115a08abf7ff17012a8
Per Thykjaer Jensen (PETJ) <asathoor@gmail.com>
2019-02-01 10:08:56 +0100

    better write to html element

--- dates/js/dates.js
+++ dates/js/dates.js
@@ -2,16 +2,26 @@
  * file: js.js
  * purpose: lists and the date object + bonus: write html to an element
  **/
-console.log('Stardate 235678.89: JavaScript from js/js.js is up and running.');
+console.log('Stardate 235678.89: JavaScript from js/dates.js is up and running.');
 
 // add the days of the week in your language as a list
 
 // @url: https://www.w3schools.com/js/js_date_methods.asp
 
+/** HOW TO ADD ANY HTML TO A TAG **/
+var myText = document.getElementById('content');
+myText.innerHTML += '<h2>The Exorbitant Shop</h2>';
+// What is the difference between += and + ?
+// Try this: add an image
+
+/** THE DATE OPBJECT **/
 let now = new Date();
 let day = now.getDay();
-
-console.log(day);
+let hour = now.getHours();
+console.log('The day is: ' + day);
+console.log('The hour is: ' + hour);
+// after a similar method you can get the month, time etc.
+// see: https://www.w3schools.com/js/js_date_methods.asp
 
 // translate this to your language (unfortunately this is Klingon)
 let week = [
@@ -28,13 +38,13 @@ let week = [
 console.log(week[day]);
 
 /* IS THE SHOP OPEN BY NOW? */
-
 // Tip: here is kind of the logic that you need ...
 var x = 1;
 
 if (x == 1){
   var open = 'Open now - today till 16.00';
   console.log(open);
+
 }
 else {
   var open = 'The shop is closed';
@@ -42,10 +52,4 @@ else {
 }
 
 /** LOOP **/
-
 // loop out the names of the days of the week
-
-/** write to an html element */
-// ( document.getElementById(id).innerHTML = new HTML )
-// var text = document.getElementById('content')
-// text.innerHTML = open;
