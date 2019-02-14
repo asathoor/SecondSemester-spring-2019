87b1ca98a1256f64f2f9201aec07d6727fa27ba0
Per Thykjaer Jensen (PETJ) <asathoor@gmail.com>
2019-02-14 10:45:10 +0100

    ignore Mozart
    idea : add img tags to the DOM.

--- video/README.md
+++ video/README.md
@@ -1,8 +1,6 @@
-Video
-=====
+JavaScript Boilerplates
+=======================
 
-## or DOM continued
+In this directory you'll find several JavaScript boilterplates.
 
-* video.html = attributes and video control via Js.
-* video02.html = video tag created on the fly via Js.
-* domFun.html = an experiment just for fun. Create a random number of animated lines, i.e. `<div>`s, created on the fly by Js.
+* jsBoilerplate.html: Vanilla JavaScript
--- video/js/domFun.js
+++ video/js/domFun.js
@@ -8,12 +8,21 @@
    this.h = window.innerHeight;
 
    for (i=1;i<Math.floor( (Math.random() * 200) + 7 );i++){
+
      this.myDot = document.createElement("div"); // create a div tag
      this.myDot.style.top = Math.floor( Math.random() * this.h -24)  +  "px";
      this.myDot.style.left = Math.floor( Math.random() * this.w -24)  +  "px";
      this.myDot.id = 'chaos_' + i; // give it an id
      this.myDot.classList.add("dots");
      this.result.appendChild(this.myDot); // append to #vid
+
+     // mozart
+     this.mozart  = document.createElement("img");
+     this.mozart.src = "images/mozart.png";
+     this.mozart.alt = "Mozart.";
+     this.mozart.style.top = Math.floor( Math.random() * this.h -24)  +  "px";
+     this.mozart.style.left = Math.floor( Math.random() * this.w -24)  +  "px";
+     this.result.appendChild(this.mozart); // append to #vid
    }
  }
 
